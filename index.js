ad016461383b54d8290bd67a4334ed204ef9ba26
programadorisgod <catapiascastro@gmail.com>
2023-06-28 16:56:01 -0500

    fix(limiter): agregue limitaciones en la cantidad de peticiones por tiempo

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ import express, { urlencoded } from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import colors from 'colors'
+import { rateLimit } from 'express-rate-limit'
+
 import connectDB from './config/conectDatabase.js'
 import routerUser from './src/routes/users/user.js'
 import routerAuth from './src/routes/auth/login.js'
 import routerFile from './src/routes/file/getFile.js'
 import swaggerDocs from './src/routes/swagger.js'
 dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT || 4000
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+})
 
 app.use(cors())
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
-
+app.use(limiter)
 app.use(routerUser)
 app.use(routerAuth)
 app.use(routerFile)
+
+app.get('/', (req, res) => {
+  res.send('API REST for DriveHub')
+})
 
 connectDB()
 const Server = app.listen(PORT, () => {
