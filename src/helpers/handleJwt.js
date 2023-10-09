bcbe4b873d6b4adf4bc87bf09c4260f536d9c424
CamiloDev <catapiascastro@gmail.com>
2023-10-09 09:24:24 -0500

    Respaldo

--- src/helpers/handleJwt.js
+++ src/helpers/handleJwt.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.secretKey, { expiresIn: '2h' })
+  return jwt.sign({ id: user._id }, process.env.secretKey)
 }
 
 export const validateToken = async (token) => {
