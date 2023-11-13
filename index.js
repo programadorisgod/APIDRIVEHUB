7767f3d5855b1b8b02f77652f9af37c97af08c3a
CamiloDev <catapiascastro@gmail.com>
2023-11-13 15:27:07 -0500

    -mfix(finished x34)

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express, { urlencoded } from 'express'
 import cors from 'cors'
 // eslint-disable-next-line no-unused-vars
 import colors from 'colors'
-import cluster from 'node:cluster'
-import os from 'node:os'
 
 import connectDB from './config/conectDatabase.js'
 import routerUser from './src/routes/users/user.js'
@@ -15,39 +13,24 @@ import morgan from 'morgan'
 
 const app = express()
 const PORT = process.env.PORT ?? 4000
-const numCPUs = os.availableParallelism()
-let Server
 
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`)
-  // Fork workers.
-  for (let index = 0; index < numCPUs; index++) {
-    cluster.fork()
-  }
+app.use(cors())
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to DRIVEHUB' })
+})
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`)
-  })
-} else {
-  app.use(cors())
-  app.use(express.json())
-  app.use(urlencoded({ extended: true }))
-  app.use(morgan('dev'))
-  app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to DRIVEHUB' })
-  })
+app.use(routerUser)
+app.use(routerAuth)
+app.use(routerFile)
+app.use(emailRouter)
+connectDB()
 
-  app.use(routerUser)
-  app.use(routerAuth)
-  app.use(routerFile)
-  app.use(emailRouter)
-  connectDB()
-
-  Server = app.listen(PORT, () => {
-    console.log(`Server running in the port: ${PORT}`.bold)
-    swaggerDocs(app, PORT)
-  })
-  console.log(`Worker ${process.pid} started`)
-}
+const Server = app.listen(PORT, () => {
+  console.log(`Server running in the port: ${PORT}`.bold)
+  swaggerDocs(app, PORT)
+})
 
 export { app, Server }
