21999424c8c0c18e229c80ffbe761a35afb5d9a0
programadorisgod <catapiascastro@gmail.com>
2025-11-24 18:16:28 -0500

    Use UNIDAD_PATH and fix file lookup recursion

--- src/helpers/files/findFile.js
+++ src/helpers/files/findFile.js
@@ -1,26 +1,23 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { UNIDAD_PATH } from '../directories/paths.js'
 
-const __dirname = fileURLToPath(import.meta.url)
 
 export default async function verifyFileExistLink(nameFile, dir) {
-  const route = path.join(__dirname, `../../../unidad/${dir}`)
-  const filesInDirectory = await fs.readdir(path.dirname(route))
+  const route = path.join(UNIDAD_PATH, `${dir}`)
+  const filesInDirectory = await fs.readdir(route)
   try {
-    // eslint-disable-next-line no-unused-vars
     for (const file of filesInDirectory) {
-      const routeFile = path.join(route, nameFile)
-      const stats = await fs.stat(routeFile)
+      const pathFile = path.join(route, file)
+      const stats = await fs.stat(pathFile)
 
       if (stats.isFile()) {
-        return routeFile
+        return pathFile
       }
+
       if (stats.isDirectory()) {
-        const result = verifyFileExistLink(nameFile, dir)
-        if (result) {
-          return result
-        }
+        const nextDir = path.join(dir, file)
+        return verifyFileExistLink(nameFile, nextDir)
       }
     }
   } catch (error) {
