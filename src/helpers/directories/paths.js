8f36ca4301cc8890007efaf5b5ce4d0e49ba2e98
programadorisgod <catapiascastro@gmail.com>
2025-11-25 07:51:27 -0500

    Add ROUTES_PATH to directory paths

--- src/helpers/directories/paths.js
+++ src/helpers/directories/paths.js
@@ -6,3 +6,4 @@ export const APP_ROOT = path.join(__dirname, '..', '..', '..') // sale de /src
 export const UNIDAD_PATH = path.join(APP_ROOT, 'unidad')
 export const UPLOADS_PATH = path.join(APP_ROOT, 'uploads')
 export const CONVERTED_ṔATH = path.join(APP_ROOT, 'converted')
+export const ROUTES_PATH = path.join(APP_ROOT, 'src', 'routes')
